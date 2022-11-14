4edc9cda4653fec8c3612b011af9cff7df07d47e
Branislav Pop <branislav.pop04@gmail.com>
2022-11-14 02:24:30 +0100

    Zadanie4 - encryption

--- src/main/java/com/example/springz23/utilities/MyKeyPairGenerator.java
+++ src/main/java/com/example/springz23/utilities/MyKeyPairGenerator.java
@@ -1,13 +1,7 @@
 package com.example.springz23.utilities;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.security.*;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 public class MyKeyPairGenerator {
     public static PrivateKey aPrivate;
@@ -23,30 +17,7 @@ public class MyKeyPairGenerator {
         aPublic = kp.getPublic();
 
         System.out.println("Private key: " + aPrivate.getFormat());
-        // prints "Private key format: PKCS#8" on my machine
 
         System.out.println("Public key: " + aPublic.getFormat());
-        // prints "Public key format: X.509" on my machine
-    }
-
-    private static PrivateKey loadPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-
-        File privateKeyFile = new File("key.priv");
-        byte[] privateKeyBytes = Files.readAllBytes(privateKeyFile.toPath());
-
-        KeyFactory privateKeyFactory = KeyFactory.getInstance("RSA");
-        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-        PrivateKey privateKey = privateKeyFactory.generatePrivate(privateKeySpec);
-        return privateKey;
-    }
-
-    private static PublicKey loadPublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        File publicKeyFile = new File("key.pub");
-        byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
-
-        KeyFactory publicKeyFactory = KeyFactory.getInstance("RSA");
-        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-        PublicKey publicKey = publicKeyFactory.generatePublic(publicKeySpec);
-        return publicKey;
     }
 }
